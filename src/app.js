67a51c68b8eaad8e55ed277a0571c87675109639
Kenneth Severe <kennethsevere916@gmail.com>
2023-10-07 23:51:48 -0400

    added new components

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React  from 'react';
+import Navbar from './components/Navbar.js';
+import AboutUs from './components/aboutus.js';
+import Home from './components/home.js'; 
+import Resources from './components/Resources.js';
+import Contact from './components/contact.js';
 
-import React, { useState }  from 'react';
-import Navbar from '/Users/kennyverse/Digitalcraft/Mental health project/fultonmindmatters/src/components/Navbar.js'
-const App = () => {
-  return <Navbar />
+export default function App() {
 
-};
+  return (
+    <BrowserRouter>
 
-export default App;
+
+      <Navbar />
+      // issues with route aboutus isnt showing info
+      <Routes>
+      
+        <Route path="/home.js" element={<Home />} />
+        
+        <Route path="/aboutus.js" element={<AboutUs />} />
+        
+        <Route path="/Resources.js" element={<Resources />} />
+        
+        <Route path="/contact.js" element={<Contact />} />
+        
+      </Routes>
+    
+    </BrowserRouter>
+  );
+
+}
+  
