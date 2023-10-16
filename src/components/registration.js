b7e305dc9e06268393b1eb5d5e3049b91650cbf6
Kenneth Severe <kennethsevere916@gmail.com>
2023-10-16 14:02:38 -0400

    ended backend

---
+++ src/components/registration.js
@@ -0,0 +1,48 @@
+// Registration.js
+import React, { useState } from 'react';
+
+const Registration = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('Registration data submitted:', formData);
+ 
+  };
+
+  return (
+    <div>
+      <h2>Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" name="username" value={formData.username} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={formData.password} onChange={handleInputChange} required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
